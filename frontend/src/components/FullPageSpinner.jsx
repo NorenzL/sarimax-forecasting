fde0b5a896a15ddb70cdd288542496dc0fb34c4e
Norenz Laurito <laurito.marknorenz@gmail.com>
2025-05-17 22:42:09 +0800

    Add loading text

--- frontend/src/components/FullPageSpinner.jsx
+++ frontend/src/components/FullPageSpinner.jsx
@@ -1,7 +1,8 @@
 export default function FullPageSpinner() {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center z-50">
       <div className="w-16 h-16 border-4 border-t-transparent border-primary rounded-full animate-spin"></div>
+      <p className="mt-4 text-white text-lg">Generating Forecast...</p>
     </div>
   );
 }
